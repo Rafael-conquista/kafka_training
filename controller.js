daca29bc554e6f0dd895a5f6eb1b5b9ed63e2c69
Rafael Conquista <rafaelconquista2002@hotmail.com>
2024-07-23 20:27:10 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,10 +1,19 @@
 import KafkaConfig from "./config.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const sendMessageToKafka = async (req, res) => {
   try {
-    const { message } = req.body;
+    const mail_desc = req.body;
+    const message = {
+      "message": mail_desc.body,
+      "email": mail_desc.email,
+      "subject": mail_desc.subject,
+    }
+    const stringJson = JSON.stringify(message);
     const kafkaConfig = new KafkaConfig();
-    const messages = [{ key: "key1", value: message }];
+    const messages = [{ key: "key1", value: stringJson}];
     kafkaConfig.produce("my-topic", messages);
 
     res.status(200).json({

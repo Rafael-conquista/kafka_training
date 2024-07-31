688e078692378772254bc902e5b83d4d981970b2
Rafael Conquista <rafaelconquista2002@hotmail.com>
2024-07-30 21:04:20 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,30 +1,49 @@
 import KafkaConfig from "./config.js";
 import dotenv from 'dotenv';
+import pkg from 'pg';
+const { Client } = pkg;
 
 dotenv.config();
 
-const sendMessageToKafka = async (req, res) => {
+const sendMessageToKafka = async (messageDB) => {
   try {
-    const mail_desc = req.body;
     const message = {
-      "message": mail_desc.body,
-      "email": mail_desc.email,
-      "subject": mail_desc.subject,
+      "message": messageDB.mensagem,
+      "email": messageDB.email,
+      "subject": "Lembrete de suas metas",
     }
     const stringJson = JSON.stringify(message);
     const kafkaConfig = new KafkaConfig();
-    const messages = [{ key: "key1", value: stringJson}];
+    const messages = [{ key: "key1", value: stringJson }];
     kafkaConfig.produce("my-topic", messages);
 
-    res.status(200).json({
-      status: "Ok!",
-      message: "Message successfully send!",
-    });
   } catch (error) {
     console.log(error);
   }
 };
 
-const constrollers = { sendMessageToKafka };
+async function chamarProcedure() {
+  const connectionString = "postgresql://postgres:example@localhost:5432/postgres";
+  const client = new Client({
+    connectionString: connectionString,
+  });
+
+  try {
+    await client.connect();
+    await client.query('CALL atualizar_notificacoes()');
+
+    console.log('Procedure chamada com sucesso!');
+
+    const messages = await client.query('select * from "NotificationMessages"')
+    return messages.rows
+  } catch (err) {
+    console.error('Erro ao chamar a procedure:', err.stack);
+  } finally {
+    await client.end();
+  }
+}
+
+const constrollers = { sendMessageToKafka, chamarProcedure };
 
 export default constrollers;
+
